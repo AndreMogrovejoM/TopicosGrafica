00483049dd669760ac2dd2a4b69a567433fda4a2
Jelaque <jlazoq@unsa.edu.pe>
2020-05-22 00:02:13 -0500

    cmake files

---
+++ Practica1/CMakeFiles/OpsImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpsImage.dir/main.cpp.o"
+  "OpsImage"
+  "OpsImage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpsImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
