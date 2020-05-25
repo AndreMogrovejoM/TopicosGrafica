dfb5c24b948acf2dde4b84a6824f4cbbde067192
Jelaque <jlazoq@unsa.edu.pe>
2020-05-25 14:30:31 -0500

    suma de columnas

--- Clase3/main.cu
+++ Clase3/main.cu
@@ -1,4 +1,4 @@
-//#include "shared.cuh"
+#include "shared.cuh"
 #include <stdio.h>
 
 #define ROW 1024
@@ -6,23 +6,27 @@
 
 
 int main(int argc, char** argv){
-  int row,col;
 
-  if (argc == 3){ //case input order: row col
-    row = atoi(argv[1]);
-    col = atoi(argv[2]);
+  int N, M;
+
+  /*if (argc == 3){ //case input order: row col
+    N = atoi(argv[1]);
+    M = atoi(argv[2]);
   }
   else if (argc == 2){ //case number cols = number rows
-    row = col = atoi(argv[1]);
+    N = N = atoi(argv[1]);
   }
   else if (argc > 3){ //error case
     printf("Error typing dimension of a matrix (rows,columns), entered more than two values\n");
     return -1;
   }
   else{ //run example with predefined size 1024x512
-    row = ROW;
-    col = COL;
-  }
+    N = ROW;
+    M = COL;
+  }*/
+  N = ROW;
+  M = COL;
+
 
   int nDevices;
 
@@ -34,12 +38,33 @@ int main(int argc, char** argv){
     printf("  Device name: %s\n", prop.name);
     printf("  Shared Memory per block: %d\n",
            prop.sharedMemPerBlock);
-    printf("  Registers per block: %d\n",
-           prop.regsPerBlock);
+    printf("  Dimension of a grid: (%d,%d,%d)\n",
+           prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
     printf("  Warp size: %d\n",
            prop.warpSize);
     printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
            2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
   }
+
+  int *Mh;
+  int *Nh;
+
+  Mh = new int[N*M];
+
+  for (int i = 0; i < N; ++i)
+    for (int j = 0; j < M; ++j)
+      Mh[i*M + j] = i;
+
+  Nh = new int[M];
+
+  sumColsMatrix(Mh,Nh,N,M);
+
+  for (int i = 0; i < M; ++i)
+    printf("%d ",Nh[i]);
+  printf("\n");
+
+  delete Mh;
+  delete Nh;
+
   return 0;
 }
